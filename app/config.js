d3a9c39aa754fcf354b645434bf01c54d22ea21e
ooohmygoosh <yihap24@gmail.com>
2026-03-20 18:01:20 +0800

    chore(app): fallback to ip api endpoint

--- app/config.js
+++ app/config.js
@@ -4,6 +4,6 @@ const extra = Constants.expoConfig?.extra || {};
 const envBase = process.env.EXPO_PUBLIC_API_BASE;
 
 const DEV_API_BASE = "http://10.0.2.2:8080";
-const PROD_API_BASE = "https://api.ooooooo0hmygooooooo0sh.xyz/api";
+const PROD_API_BASE = "http://47.98.253.11/api";
 
 export const API_BASE = envBase || extra.apiBase || (__DEV__ ? DEV_API_BASE : PROD_API_BASE);
